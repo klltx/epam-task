c1803ba1790fad11e46c99401c0220f6ccffa80d
klltx <kostya.svetashov.01@gmail.com>
2021-05-20 10:06:06 +0400

    feat: deep copy for array

--- deepCopy.js
+++ deepCopy.js
@@ -1,6 +1,6 @@
 const deepCopy = function(object) {
   if(typeof object === 'object') {
-    const copy = {...object};
+    const copy = Array.isArray(object) ? [...object] : {...object};
     for (const key in copy) copy[key] = deepCopy(copy[key]);
     return copy;
   }
